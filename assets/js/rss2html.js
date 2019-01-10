9b62c83f39f1afdebf0c8d5bd5803f1776d26fa3
yosuke <yosuke@cowcowhomes.webcrow.jp>
2019-01-10 18:00:03 +0900

    同じドメインでRSSの解析をするようにした

---
+++ assets/js/rss2html.js
@@ -0,0 +1,27 @@
+window.addEventListener( 'load', function(){
+	$.ajax({
+		type: 'GET',
+		url: '/rss/index.php',
+		dataType: 'json',
+		success: function(json){
+			console.log(json["item"]);
+			var rss_items = json["item"];
+
+			var len = rss_items.length;
+			if(len > 10)
+				len = 10;
+
+			$(".rss-box").append("<ul class='rss-items'></ul>");
+
+			var new_ul = $(".rss-box .rss-items");
+
+			for(var i=0; i < rss_items.length; i++){
+				var html = "<li class='rss-item'><a href='%url%'>%title%</a></li>";
+				var str = html.replace("%url%",rss_items[i]["link"]);
+				var str = str.replace("%title%",rss_items[i]["title"]);
+				console.log(str);
+				new_ul.append(str);
+			}
+		}
+	});
+},false);
---
+++ rss/index.php
@@ -0,0 +1,50 @@
+<?php
+
+$url = "https://tokyo.craigslist.org/search/apa?availabilityMode=0&format=rss&query=cowcowhomes%20LTD&sort=date";
+
+$ch = curl_init(); // 初期化
+curl_setopt( $ch, CURLOPT_URL, $url ); // URLの設定
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); // 出力内容を受け取る設定
+$result = curl_exec( $ch ); // データの取得
+curl_close($ch); // cURLのクローズ
+
+$strJson = xml_to_json($result);
+
+echo $strJson;
+
+//**********************************
+// XML ⇒ JSONに変換する関数
+//**********************************
+function xml_to_json($xml)
+{
+    // コロンをアンダーバーに（名前空間対策）
+    $xml = preg_replace("/<([^>]+?):([^>]+?)>/", "<$1_$2>", $xml);
+    // プロトコルのは元に戻す
+    $xml = preg_replace("/_\/\//", "://", $xml);
+    // XML文字列をオブジェクトに変換（CDATAも対象とする）
+    $objXml = simplexml_load_string($xml, NULL, LIBXML_NOCDATA);
+    // 属性を展開する
+    xml_expand_attributes($objXml);
+    // JSON形式の文字列に変換
+    $json = json_encode($objXml, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    // "\/" ⇒ "/" に置換
+    return preg_replace('/\\\\\//', '/', $json);
+}
+
+//**********************************
+// XMLタグの属性を展開する関数
+//**********************************
+function xml_expand_attributes($node)
+{
+    if($node->count()) {
+        foreach($node->children() as $child)
+        {
+            foreach($child->attributes() as $key => $val) {
+                $node->addChild($child->getName()."@".$key, str_replace('&', '&amp;', $val));
+            }
+            xml_expand_attributes($child); // 再帰呼出
+        }
+    }
+}
+
+?>
