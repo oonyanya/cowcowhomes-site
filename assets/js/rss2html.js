6b2f49261e4f6d5d98feb7223b547d70e5347334
yosuke <yosuke@cowcowhomes.webcrow.jp>
2019-02-28 16:39:51 +0900

    data属性でRSSフィードのURLを指定できるようにした

--- assets/js/rss2html.js
+++ assets/js/rss2html.js
@@ -1,47 +1,55 @@
+function addRssItem(json)
+{
+	if(json.length == 0)
+		return;
+
+	var rss_items = json["item"];
+
+	var len = rss_items.length;
+	if(len > 10)
+		len = 10;
+
+	var root = document.getElementsByClassName('rss-items')[0];
+
+	var template = document.getElementById('rss-template');
+
+	for(var i=0; i < len; i++){
+		//複製してliタグを得る
+		var new_li = document.importNode(template.content, true).firstElementChild;
+		var new_a = new_li.firstElementChild;
+
+		new_a.setAttribute("href", rss_items[i]["link"]);
+		new_a.innerHTML = rss_items[i]["title"];
+
+		var m = rss_items[i]["title"].match(/\((.+)\)/);
+		if(m)
+			var view_item_text = m[1].trim();
+		else
+			var view_item_text = "craiglist_item";
+
+		var gtag_text = "gtag('event','view_item', {'id': '%id%', 'name': '%text%'})";
+		gtag_text = gtag_text.replace("%text%",view_item_text);
+		gtag_text = gtag_text.replace("%id%", Math.floor(Math.random() * 32767));
+		new_a.setAttribute("onclick" , gtag_text );
+
+		root.appendChild(new_li);
+	}
+
+	var summary = document.getElementsByClassName('summary')[0];
+	summary.setAttribute("href", json["channel"]["link"]);
+	summary.innerHTML = json["channel"]["title"];
+}
+
 window.addEventListener( 'load', function(){
-	$.ajax({
-		type: 'GET',
-		url: '/rss/index.php?rss_url=https%3A%2F%2Ftokyo.craigslist.org%2Fsearch%2Fhhh%3FavailabilityMode%3D0%26format%3Drss%26query%3Dcowcowhomes%2BLTD%26sort%3Ddate%26lang%3Den%26cc%3Dus',
-		dataType: 'json',
-		success: function(json){
-			if(json.length == 0)
-				return;
-
-			var rss_items = json["item"];
-
-			var len = rss_items.length;
-			if(len > 10)
-				len = 10;
-
-			var root = document.getElementsByClassName('rss-items')[0];
-
-			var template = document.getElementById('rss-template');
-
-			for(var i=0; i < len; i++){
-				//複製してliタグを得る
-				var new_li = document.importNode(template.content, true).firstElementChild;
-				var new_a = new_li.firstElementChild;
-
-				new_a.setAttribute("href", rss_items[i]["link"]);
-				new_a.innerHTML = rss_items[i]["title"];
-
-				var m = rss_items[i]["title"].match(/\((.+)\)/);
-				if(m)
-					var view_item_text = m[1].trim();
-				else
-					var view_item_text = "craiglist_item";
-
-				var gtag_text = "gtag('event','view_item', {'id': '%id%', 'name': '%text%'})";
-				gtag_text = gtag_text.replace("%text%",view_item_text);
-				gtag_text = gtag_text.replace("%id%", Math.floor(Math.random() * 32767));
-				new_a.setAttribute("onclick" , gtag_text );
-
-				root.appendChild(new_li);
-			}
-
-			var rssbox = document.getElementsByClassName('summary')[0];
-			rssbox.setAttribute("href", json["channel"]["link"]);
-			rssbox.innerHTML = json["channel"]["title"];
-		}
-	});
+	var rssboxs = document.getElementsByClassName('rss-box');
+	for(var i = 0; i < rssboxs.length; i++)
+	{
+		var rss_url = rssboxs[i].getAttribute("data-rss-url");
+		$.ajax({
+			type: 'GET',
+			url: "/rss/index.php?rss_url=" + rss_url,
+			dataType: 'json',
+			success: addRssItem
+		});
+	}
 },false);
