b7a84c6617d9e4c5c1b193b265c7e8205f699b7e
rirufa <test@yahoo.co.jp>
2024-03-22 12:16:30 +0900

    アップロードの仕方を変更した

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,5 @@
 @call hexo clean
 @call hexo gzip
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" %FTP_SERVER_HOST% /synchronize .\public /
+call node web_fetch.js
+"C:\Program Files (x86)\WinSCP\WinSCP.exe" /console /script=deploy_web.txt /parameter %FTP_SERVER_HOST% %~dp0%/public
 .\gdrive.exe sync upload .\source\image\ 1gMOz4dX75PRZ4E1DyHftNw7xGZOKIO3w
