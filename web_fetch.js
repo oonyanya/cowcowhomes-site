b63a87ef0ba94cff3a78ef3be006c546f3cfa3b8
rirufa <test@yahoo.co.jp>
2024-03-22 17:47:38 +0900

    UserAgentを変更した

--- web_fetch.js
+++ web_fetch.js
@@ -15,7 +15,20 @@ const limit = 10;
 
 async function crawl(url) {
   try {
-    const response = await axios.get(url);
+    const headers = {
+      'Cache-Control':'no-cache',
+      'Accept':'text/html,application/xhtml+xml,application/xml',
+      'Accept-Language': 'ja,en',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0',
+      'Sec-Ch-Ua':'Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122',
+      'Sec-Ch-Ua-Mobile':'?0',
+      'Sec-Ch-Ua-Platform':"Windows",
+      'Sec-Fetch-Dest':'document',
+      'Sec-Fetch-Mode':'navigate',
+      'Sec-Fetch-Site':'cross-site',
+      'Sec-Fetch-User':'?1'
+    }
+    const response = await axios.get(url,{headers});
     const $ = cheerio.load(response.data);
     let urlTitleList = [];
 
