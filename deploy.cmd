bca6bc9bd107286491f48f5cd90a89b9662c8f2e
rirufa <test@yahoo.co.jp>
2024-09-05 17:50:38 +0900

    google driveがコマンドラインから使えなくなったのでgitで管理することにした

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,2 @@
 @call build.cmd
 "C:\Program Files (x86)\WinSCP\WinSCP.exe" /console /script=deploy_web.txt /parameter %FTP_SERVER_HOST% %~dp0%/public
-.\gdrive.exe sync upload .\source\image\ 1gMOz4dX75PRZ4E1DyHftNw7xGZOKIO3w
